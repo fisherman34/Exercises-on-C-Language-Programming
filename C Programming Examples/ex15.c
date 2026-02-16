35d8a50fb7b296a7531a8778a2654854dc94ffbd
Sam Xu <fisherman34@qq.com>
2026-02-16 23:56:24 +0900

    2.16 exer

---
+++ C Programming Examples/ex15.c
@@ -0,0 +1,26 @@
+/*
+判断输入的字母是元音，还是辅音。
+英语有26个字母，元音只包括 a、e、i、o、u 这五个字母，
+其余的都为辅音。y是半元音、半辅音字母，但在英语中都把他当作辅音。
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  char c;
+  int isLowercaseVowel, isUppercaseVowel;
+
+  printf("输入一个字母： ");
+  scanf("%c", &c);
+
+  isLowercaseVowel = ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+  
+  isUppercaseVowel = ( c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+  if( isLowercaseVowel || isUppercaseVowel)
+    printf("%c 是元音", c);
+  else
+    printf("%c 是辅音", c);
+  return 0;
+}
